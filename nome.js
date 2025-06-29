4624cc3ae8b7f44dbc524e53627f753d80c32427
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-06-29 14:29:08 -0300

    Arrumando
    Dando uma arrumada no projeto

--- nome.js
+++ nome.js
@@ -1,5 +1,32 @@
 function cadastro(){
-var c =window.document.querySelector('#site')
-    var c.style.display ='block'
 
+    let site = window.document.querySelector('#site')
+    let bloco = window.document.querySelector('#bloco')
+    const nome = window.document.querySelector('#name')
+    let area = window.document.querySelector('#areanome')
+    const sex = window.document.getElementsByName('radsex')
+    const image = window.document.querySelector('img#img')
+    let nada = window.document.querySelector('#nada')
+
+
+    if( nome.value.length == 0){
+        window.alert('NOME INEXISTENTE!!')
+    }else{
+        site.style.display = 'block'
+        bloco.style.display = 'none';
+        bloco.style.transition = ' opacity 0.2s';
+    
+        area.innerHTML = `${nome.value}`
+        area.style.background = 'white'
+    }
+
+    if(sex[0].checked){
+        image.src = 'perfil.png'
+    }else if(sex[1].checked){
+        image.src = 'mm.avif';
+        nada.style.background = '#5bc287'
+    }else if(sex[2].checked){
+         image.src = 'wolf1.jpg'
+    }
 }
+
