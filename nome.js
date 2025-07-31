003e965d2bbc3cdce983b21ce2a7bbe5ded4b138
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-07-31 10:04:19 -0300

    melhorando bugs

--- nome.js
+++ nome.js
@@ -1,32 +1,30 @@
-function cadastro(){
+const sex = window.document.getElementsByName('radsex');
+const image = window.document.querySelector('img#img');
+const nome = window.document.querySelector('#name');
 
-    let site = window.document.querySelector('#site')
-    let bloco = window.document.querySelector('#bloco')
-    const nome = window.document.querySelector('#name')
-    let area = window.document.querySelector('#areanome')
-    const sex = window.document.getElementsByName('radsex')
-    const image = window.document.querySelector('img#img')
-    let nada = window.document.querySelector('#nada')
+let site = window.document.querySelector('#site');
+let bloco = window.document.querySelector('#bloco');
+let area = window.document.querySelector('#areanome');
+let areaftperfil = window.document.querySelector('#areaftperfil');
 
+const cadastro = window.document.querySelector('#cadastro');
 
-    if( nome.value.length == 0){
-        window.alert('NOME INEXISTENTE!!')
-    }else{
-        site.style.display = 'block'
-        bloco.style.display = 'none';
-        bloco.style.transition = ' opacity 0.2s';
+cadastro.addEventListener('click', () =>{
+    site.style.display = 'block';
+    bloco.style.display = 'none';
+    bloco.style.transition = ' opacity 0.2s';
     
-        area.innerHTML = `${nome.value}`
-        area.style.background = 'white'
-    }
-
+    area.innerHTML = `${nome.value}`;
+    area.style.background = 'white';
+    
+    
+    
     if(sex[0].checked){
         image.src = 'perfil.png'
     }else if(sex[1].checked){
         image.src = 'mm.avif';
-        nada.style.background = '#5bc287'
+        areaftperfil.style.background = '#5bc287'
     }else if(sex[2].checked){
-         image.src = 'wolf1.jpg'
+        image.src = 'wolf1.jpg'
     }
-}
-
+})
