dea4d85b55334ad425939fcb89b71b469437de65
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-08-01 11:37:55 -0300

    upgrade

--- nome.js
+++ nome.js
@@ -10,21 +10,28 @@ let areaftperfil = window.document.querySelector('#areaftperfil');
 const cadastro = window.document.querySelector('#cadastro');
 
 cadastro.addEventListener('click', () =>{
-    site.style.display = 'block';
-    bloco.style.display = 'none';
-    bloco.style.transition = ' opacity 0.2s';
-    
-    area.innerHTML = `${nome.value}`;
-    area.style.background = 'white';
-    
-    
-    
-    if(sex[0].checked){
-        image.src = 'perfil.png'
-    }else if(sex[1].checked){
-        image.src = 'mm.avif';
-        areaftperfil.style.background = '#5bc287'
-    }else if(sex[2].checked){
-        image.src = 'wolf1.jpg'
+    if(nome.value == 0){
+        window.alert('Coloque um Name válido')
+    }else{
+        site.style.display = 'block';
+        bloco.style.display = 'none';
+        bloco.style.transition = ' opacity 0.2s';
+        
+        area.innerHTML = `${nome.value}`;
+        area.style.background = 'white';
+        
+        
+        
+        if(sex[0].checked){
+            image.src = 'perfil.png'
+        }else if(sex[1].checked){
+            image.src = 'mm.avif';
+            areaftperfil.style.background = '#5bc287'
+        }else if(sex[2].checked){
+            image.src = 'wolf1.jpg'
+        }
     }
+
 })
+
+
